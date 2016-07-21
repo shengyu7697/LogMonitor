8ad7f477ed0b4365d40c568e725087411bbbaf30
shengyu <shengyu7697@gmail.com>
2016-07-21 21:18:25 +0800

    add usage and check file exist

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <string>
+#include <string.h>
 #include "LogMonitor.h"
 #if defined(_WIN32)
 #include <Windows.h>
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
 #else
 #include <unistd.h>
+#include <sys/stat.h>
 #endif
 
 using namespace std;
@@ -24,12 +27,39 @@ void systemPause()
     getchar();
 }
 
+void help(const char *progname)
+{
+    printf("\nThis program is used to monitor log\n"
+            "Usage:\n"
+            "%s log_filename\n\n", progname);
+}
+
+bool fileExists(const char *filename)
+{
+#if defined(_WIN32)
+    return (PathFileExists(filename) == TRUE);
+#else
+    struct stat sb;
+    return (stat(filename, &sb) == 0);
+#endif
+}
+
 int main(int argc, const char *argv[])
 {
-    string filename = "test.log";
+    string filename;
+    bool exists = false;
 
-    if (argc >= 2)
-        filename = argv[1];
+    if (argc == 2)
+    {
+        if ((exists = fileExists(argv[1])))
+            filename = argv[1];
+    }
+
+    if (!exists)
+    {
+        help(argv[0]);
+        return 1;
+    }
 
     LogMonitor lm(filename.c_str());
 
